12510f7cbc687b79beb2d09f1fedb533f4358902
Swapnil2411 <swapnildarge65@gmail.com>
2026-03-31 11:27:50 +0530

    Projects Section done.

--- app/home/projectSection/ProjectContent.tsx
+++ app/home/projectSection/ProjectContent.tsx
@@ -1,7 +1,57 @@
+"use client"
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
 export default function ProjectContent(){
+    const projectData = [
+        "isro",
+        "bmc",
+        "npcil",
+        "mahindra",
+        "sameer",
+        "barc"
+    ]
     return(
         <div className="project_content_wrapper sec_pad">
-            
+            <Swiper
+                modules={[Pagination, Navigation]}
+                // autoplay={{ delay: 4000 }}
+                loop={true}
+                pagination={{ clickable: true }}
+                navigation={true}
+                breakpoints={{
+          '@0.00': {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          '@0.75': {
+            slidesPerView: 3,
+            spaceBetween: 10,
+          },
+          '@1.00': {
+            slidesPerView: 4,
+            spaceBetween: 10,
+          },
+          '@1.50': {
+            slidesPerView: 5,
+            spaceBetween: 10,
+          },
+        }}
+                className="h-full projects_swiper"
+            >
+                {
+                    projectData?.map((item,index)=>(
+                        <SwiperSlide key={index}>
+                            <div className="project_item">
+                                <img src={`/assets/${item}.png`} alt={item} />
+                            </div>
+                        </SwiperSlide>
+                    ))
+                }
+            </Swiper>
         </div>
     )
 }
--- app/home/projectSection/page.tsx
+++ app/home/projectSection/page.tsx
@@ -1,10 +1,12 @@
 import SectionHeading from "@/components/common/SectionHeading";
+import ProjectContent from "./ProjectContent";
 
 export default function ProjectSection(){
     return(
         <div className="project_wrapper">
             <div className="container">
                 <SectionHeading title="Projects" subtitle="Projects" />
+                <ProjectContent />
             </div>
         </div>
     )
